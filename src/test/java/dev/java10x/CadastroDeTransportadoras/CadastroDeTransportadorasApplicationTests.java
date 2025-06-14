9b7c29f084557bb7d676ce130651dc2ff15b0279
Arthur Rodrigues <127230206+ArthurPRodrigues@users.noreply.github.com>
2025-06-13 23:29:52 -0300

    first commit

---
+++ src/test/java/dev/java10x/CadastroDeTransportadoras/CadastroDeTransportadorasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeTransportadoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeTransportadorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
